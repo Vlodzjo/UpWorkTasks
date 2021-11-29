26df19050b86c6a5a6089ba8540abd995f92bfaf
Vlodzjo <volodyalarych24@gmail.com>
2021-11-29 13:06:10 +0200

    Tasks decision [fix, DEV 1]

--- src/main/java/TaskFour.java
+++ src/main/java/TaskFour.java
@@ -7,21 +7,21 @@ public class TaskFour {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(new InputStreamReader(System.in));
-        List<Integer> array = new ArrayList<>();
-        int arraySize = getIntArray();
-        for (int i = 0; i < arraySize; i++) {
-            System.out.print("Enter next whole number: ");
-            int number = scanner.nextInt();
-            array.add(number);
-        }
-        for (int number : array) {
+        System.out.print("Enter array size: ");
+        List<Integer> array = getIntArray(scanner.nextInt());
+        for (Integer number : array) {
             System.out.println(number);
         }
     }
 
-    public static int getIntArray() {
+    public static ArrayList<Integer> getIntArray(int size) {
         Scanner scanner = new Scanner(new InputStreamReader(System.in));
-        return scanner.nextInt();
+        ArrayList<Integer> array = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            System.out.print("Enter next whole number: ");
+            int number = scanner.nextInt();
+            array.add(number);
+        }
+        return array;
     }
-
 }
--- src/main/java/TaskTwo.java
+++ src/main/java/TaskTwo.java
@@ -1,27 +1,21 @@
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class TaskTwo {
 
     public static void main(String[] args) {
-        averageIntArray();
+        Scanner scanner = new Scanner(new InputStreamReader(System.in));
+        System.out.print("Enter array size: ");
+        ArrayList<Integer> array = TaskFour.getIntArray(scanner.nextInt());
+        averageIntArray(array);
     }
 
-    public static void averageIntArray() {
-        Scanner scanner = new Scanner(new InputStreamReader(System.in));
-        List<Integer> array = new ArrayList<>();
-        int arraySize = TaskFour.getIntArray();
-        for (int i = 0; i < arraySize; i++) {
-            System.out.print("Enter next whole number: ");
-            int number = scanner.nextInt();
-            array.add(number);
-        }
+    public static void averageIntArray(ArrayList<Integer> array) {
         double average = array.stream().mapToDouble(number -> number).sum();
-        average /= arraySize;
+        average /= array.size();
         System.out.println("Average: " + average);
-        for (double greater : array) {
+        for (int greater : array) {
             if (greater > average) {
                 System.out.println(greater);
             }
