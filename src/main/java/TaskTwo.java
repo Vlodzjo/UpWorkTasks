98574332cef4b6ff80d118bac644b736f599004b
Vlodzjo <volodyalarych24@gmail.com>
2021-11-29 13:55:11 +0200

    Tasks decision [fix, DEV 1.4]

--- src/main/java/TaskTwo.java
+++ src/main/java/TaskTwo.java
@@ -8,17 +8,18 @@ public class TaskTwo {
         Scanner scanner = new Scanner(new InputStreamReader(System.in));
         System.out.print("Enter array size: ");
         List<Integer> array = TaskFour.getIntArray(scanner.nextInt());
-        averageIntArray(array);
-    }
-
-    public static void averageIntArray(List<Integer> array) {
-        double average = array.stream().mapToDouble(number -> number).sum();
-        average /= array.size();
+        double average = averageIntArray(array);
         System.out.println("Average: " + average);
         for (int greater : array) {
             if (greater > average) {
                 System.out.println(greater);
             }
         }
+    }
+
+    public static double averageIntArray(List<Integer> array) {
+        double average = array.stream().mapToDouble(number -> number).sum();
+        average /= array.size();
+        return average;
     }
 }
